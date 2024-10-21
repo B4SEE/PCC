e12e32c33efc2d65a8f81effd7f9eb12dd47bcc5
Eleonora Virych <virycele@fel.cvut.cz>
2024-10-21 15:05:03 +0200

    add output for parameters --help and --version

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,8 +3,30 @@
 #include <Config.h>
 #include <sstream>
 #include <Game.h>
+#include <fstream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        std::string arg = argv[1];
+        if (arg == "--help") {
+            std::ifstream logHeader("../resources/help.txt");
+            if (logHeader.is_open()) {
+                std::string line;
+                while (std::getline(logHeader, line)) {
+                    std::cout << line << std::endl;
+                }
+                logHeader.close();
+            } else {
+                std::cerr << "Help file" << std::endl;
+            }
+            std::exit(0); // Exit after showing help
+        }
+        if (arg == "--version") {
+            std::cout << "Version 1.0" << std::endl;
+            std::exit(0); // Exit after showing version
+        }
+    }
+
     Game game;
     game.start();
     game.stop();
